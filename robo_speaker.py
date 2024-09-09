e7874ee2fcf695846f390dae4cce4df3b2ea868a
Saurabh1502 <yadavsaurabh1502@gmail.com>
2024-09-09 13:21:51 +0530

    Update robo_speaker.py
    there is some spelling mistake on robo fucntion name

--- robo_speaker.py
+++ robo_speaker.py
@@ -7,7 +7,7 @@ def robo_speak(message):
     speak = pyttsx3.init()  
     # set properties (optional)
     
-    speak.setProperty('rate',100) # speed of speech (word per minute)
+    speak.setProperty('rate',130) # speed of speech (word per minute)
     speak.setProperty('valume',1.0) # volume (0.0 to 1.0)
     # speak the message 
     speak.say(message)
@@ -18,7 +18,7 @@ robo_speak(f"hii{name}, welcome to robo speaker")
 while True:
     message = input("Enter the message here to speak: ")
     if message =="exit":
-        rob0_speak("bye bye")
+        robo_speak("bye bye")
         break
     robo_speak(message)
 
